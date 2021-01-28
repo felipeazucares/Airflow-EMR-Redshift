2083749bfb7115ff671ce18beb8605e4c1eb7f94
FelipeAzucares <phil@red-robot.biz>
2021-01-28 20:47:12 +0000

    Clear cache to get example dag to load

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -3,7 +3,7 @@ import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+                               LoadDimensionOperator, DataQualityOperator)
 from helpers import SqlQueries
 
 # AWS_KEY = os.environ.get('AWS_KEY')
@@ -18,7 +18,7 @@ dag = DAG('udac_example_dag',
           default_args=default_args,
           description='Load and transform data in Redshift with Airflow',
           schedule_interval='0 * * * *'
-        )
+          )
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
@@ -63,3 +63,6 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+start_operator >> [stage_events_to_redshift,
+                   stage_songs_to_redshift] >> load_songplays_table
