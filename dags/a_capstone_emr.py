09736f24dc61596358312b093678c2115eb28f16
FelipeAzucares <phil@red-robot.biz>
2021-02-16 22:57:49 +0000

    WIP fix for numeric colum type mismatch in SQL_queries.py

--- dags/a_capstone_emr.py
+++ dags/a_capstone_emr.py
@@ -179,7 +179,7 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=5),
+    "retry_delay": timedelta(minutes=3),
 }
 
 dag = DAG("a_capstone_emr",
