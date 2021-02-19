4c5cf6acf68857a127319e92310c1e1bfd749887
Philip Suggars <phil@red-robot.biz>
2021-02-19 12:58:56 +0000

    WIP removal of non used operators from __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,13 +6,12 @@ import operators
 import helpers
 
 # Defining the plugin class
+
+
 class UdacityPlugin(AirflowPlugin):
     name = "udacity_plugin"
     operators = [
         operators.StageToRedshiftOperator,
-        operators.LoadFactOperator,
-        operators.LoadDimensionOperator,
-        operators.DataQualityOperator
     ]
     helpers = [
         helpers.SqlQueries
