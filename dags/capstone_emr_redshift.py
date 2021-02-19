664137d40ee99feaf0272527763963951d663763
Philip Suggars <phil@red-robot.biz>
2021-02-19 22:14:03 +0000

    Final code check-in in all functioning.

--- dags/capstone_emr_redshift.py
+++ dags/capstone_emr_redshift.py
@@ -10,6 +10,7 @@
 #   5. Shutdown EMR cluster
 #   6. Creates dimension and fact tables in pre-existing redshift instance
 #   7. populates tables from processed data in S3
+#
 
 
 from pyspark.sql import functions as F
